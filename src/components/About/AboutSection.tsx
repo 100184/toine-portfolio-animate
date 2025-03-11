227bf24ea6fe579d7b77861b7b63cc3fca0bbc20
Toine <149142883+100184@users.noreply.github.com>
2025-03-11 10:58:06 +0100

    Update AboutSection.tsx

--- src/components/About/AboutSection.tsx
+++ src/components/About/AboutSection.tsx
@@ -21,7 +21,7 @@ const AboutSection: React.FC = () => {
               </div>
             ) : (
               <img 
-                src="/toineportofoto.png" 
+                src="/fotoporto.jpg" 
                 alt="Toine Louis Heijstek" 
                 className="h-full w-full object-cover"
                 onError={(e) => {
